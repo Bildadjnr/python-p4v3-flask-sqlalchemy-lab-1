475afd723809e68feaa76cc225c8fef4faaeb2db
Bildad Ereggae <bildadereggae@gmail.com>
2025-06-17 22:04:19 +0300

    lab 1 done

--- server/app.py
+++ server/app.py
@@ -21,6 +21,24 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route("/earthquakes/<int:id>")
+def get_earthquake(id):
+    earthquake = Earthquake.query.filter_by(id=id).first()
+
+    if earthquake is None:
+        return ({"message": f"Earthquake {id} not found."}), 404
+
+    return (earthquake.to_dict()), 200
+
+
+@app.route("/earthquakes/magnitude/<float:magnitude>")
+def get_earthquake_by_magnitude(magnitude):
+    earthquake = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+
+    quake_list = [quake.to_dict() for quake in earthquake]
+
+    return ({"count": len(quake_list), "quakes": quake_list}), 200
+
 
 
 if __name__ == '__main__':
